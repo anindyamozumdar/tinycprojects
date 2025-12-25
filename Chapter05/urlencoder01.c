f7e5441646df133578384b66d861167e98d008c9
Anindya Mozumdar <anindya@aninmoz.com>
2025-12-25 17:41:09 +0530

    Completed Chapter 05.

---
+++ Chapter05/urlencoder01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main (void)
+{
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '-' || ch == '.' || ch == '_' || ch == '*')
+            putchar(ch);
+        else if (ch == '+')
+            putchar('+');
+        else if (isalnum(ch))
+            putchar(ch);
+        else
+            printf("%%%02X", ch);
+    }
+    return 0;
+}
