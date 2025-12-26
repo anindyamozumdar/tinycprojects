67946743820667d71b3d81cb8bebe37e8c19f4fb
Anindya Mozumdar <anindya@aninmoz.com>
2025-12-26 11:28:48 +0530

    Started Chapter 06.

---
+++ Chapter06/brutepass01.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int flip (char *c, int pos)
+{
+    *(c + pos) = *(c + pos) + 1;
+    if (*(c + pos) > 'z') {
+        *(c + pos) = 'a';
+        if (pos != 0) {
+            if (!flip(c, pos - 1))
+                return 0;
+        }
+        else
+            return 0;
+    }
+    return 1;
+}
+
+int main (void)
+{
+    char password[] = "password";
+    char guess[] = "aaaaaaaa";
+    char *p, *g;
+
+    do {
+        p = password;
+        g = guess;
+        while (*p) {
+            if (*p == *g) {
+                p++;
+                g++;
+            }
+            else
+                break;
+        }
+        if (*p == '\0' && *g == '\0') {
+            printf("Found password '%s'\n", guess);
+            break;
+        }
+    } while (flip(guess, 7));
+    return 0;
+}
