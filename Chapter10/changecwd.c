6978aeb18c9ab4f242054671217109aff71a8974
Anindya Mozumdar <anindya@aninmoz.com>
2026-01-30 11:54:33 +0530

    Continued with Chapter 10.

---
+++ Chapter10/changecwd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (void)
+{
+    char cwd[BUFSIZ];
+    int r;
+
+    r = chdir("..");
+    if (r == -1) {
+        fprintf(stderr, "Unable to change directories\n");
+        exit(1);
+    }
+    getcwd(cwd, BUFSIZ);
+    printf("The current working directory is %s\n", cwd);
+    
+    return 0;
+}
